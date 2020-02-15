492b9176460c919fb644b2f1aa609932b0062cf3
Austin Parvin <austin.parvin@gmail.com>
2020-02-15 16:10:16 -0500

    moved everything into classes

---
+++ Deck.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace Blackjack
+{
+  public class Deck
+  {
+    public List<string> Suits { get; set; }
+
+    public List<string> Ranks { get; set; }
+
+    public List<Card> Cards { get; set; }
+
+    public Deck()
+    {
+      Cards = new List<Card>();
+      Suits = new List<string>() { "Hearts", "Clubs", "Spades", "Diamonds" };
+      Ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+      for (var i = 0; i < Suits.Count; i++)
+      {
+        for (var n = 0; n < Ranks.Count; n++)
+        {
+          var card = new Card();
+          card.Rank = Ranks[n];
+          card.Suit = Suits[i];
+          if (card.Suit == "diamonds" || card.Suit == "hearts")
+            card.Color = "red";
+          else
+            card.Color = "black";
+
+          Cards.Add(card);
+        }
+      }
+    }
+
+    public void ShuffleDeck(){
+        for (int i = Cards.Count - 1; i >= 0; i--)
+        {
+          var j = new Random().Next(Cards.Count);
+          var temp = Cards[j];
+          Cards[j] = Cards[i];
+          Cards[i] = temp;
+        }
+    }
+  }
+}
--- Player.cs
+++ Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Blackjack
@@ -5,20 +6,33 @@ namespace Blackjack
   public class Player
   {
     //Cards in the current hand
+    public string Name {get;set;}
+
     public List<Card> Hand { get; set; }
 
     // Total of the current hand
     public int HandValue { get; set; }
 
+    
 
-
-    // public void DealACard() {
-    //     DealACard() {
-    //       Hand.Add(deck[0]);
-    //       HandValue += deck[0].GetCardValue();
-    //       deck.RemoveAt(0);
+    public void DealACard( List<Card> deck) {
+   
+          Hand.Add(deck[0]);
+          HandValue += deck[0].GetCardValue();
+          deck.RemoveAt(0);
         
-    // }
+    }
+
+    public void ShowHand() {
+        for (int i = 0; i < Hand.Count; i++)
+        {
+          Console.WriteLine(Hand[i].DisplayCard());
+        }
+    }
+
+    public void ShowHandValue() {
+        Console.WriteLine($"{Name}'s current total hand value is: {HandValue}");
+    }
 
   }
 }
